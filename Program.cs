c51d7b018fc11724ab743b97141aeac24a8cb0d0
jumingyu <knighp@gmail.com>
2020-11-17 20:33:22 +0900

    1st commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGitProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
