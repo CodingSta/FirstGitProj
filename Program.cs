1e99e014e1a89e9dda4bd3bd3658cf05d97632b6
jumingyu <knighp@gmail.com>
2020-11-17 20:54:56 +0900

    2nd commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FirstGitProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello MinGyu!");
         }
     }
 }
